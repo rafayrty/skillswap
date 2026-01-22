f336da2a3ad0a8d3b33029df5c9a0b696cfb577a
Abdul Rafay <rafay@Abduls-MacBook-Air.local>
2026-01-22 06:06:12 +0500

    second

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,20 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  // Add rules
+  {
+    rules: {
+      // Disable unused vars errors
+      "@typescript-eslint/no-unused-vars": "off",
+
+      // Turn off React hook exhaustive deps warning (optional)
+      "react-hooks/exhaustive-deps": "off",
+
+      // Disable Next.js <img> warning (optional)
+      "@next/next/no-img-element": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
